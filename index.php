5330e42d034c9399ab3d0ba8e7851b5198d05c71
czaper <kacpermurat@gmail.com>
2017-02-22 12:46:19 +0100

    1

---
+++ index.php
@@ -0,0 +1,205 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta name="viewport" content="initial-scale=1.0,width=device-width" />
+    <meta charset="utf-8">
+    <title>TELEDU</title>
+    <link rel="icon" type="image/png" href="img/logo_icon.png">
+    <link href="style.css" rel="stylesheet" type="text/css"/>
+    <script src="http://code.jquery.com/jquery-2.1.3.min.js"></script>
+    <script src="myScript.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    <!-- Chrome, Firefox OS and Opera -->
+<meta name="theme-color" content="#222930">
+<!-- Windows Phone -->
+<meta name="msapplication-navbutton-color" content="#222930">
+<!-- iOS Safari -->
+<meta name="apple-mobile-web-app-status-bar-style" content="#222930">
+
+
+
+  </head>
+  <body>
+    <div class="wrapper">
+      <div class="logo_container">
+          <img class="logo" src="img/logo2.png">
+<!--
+          <form action="zaloguj.php" method="post">
+           <p>Login:</p>
+           <br/>
+           <input type="text" placeholder="Enter Username" class="my_input" name="login">
+           <br/><br/>
+           <p>Hasło:</p>
+           <br/>
+           <input type="password" placeholder="Enter Password" class="my_input" name="haslo">
+           <br/><br/>
+           <input type="submit" value="zaloguj się" class="button">
+         </form>
+-->
+
+        </div> 
+      <div class="container">
+        <div class="content">
+          <div class="tile-container">
+
+            <!-- UEC -->
+            <a href="UEC.html">
+              <div class="tile" style="background-color: #df2f00;"> <div class="inside-text">
+                <div class="image"> <img src="img/bramka.png" height="90%"></div>
+                <p style="margin-top: 3%; margin-bottom: 4%; color:#8c1d00;"><b>UKŁADY CYFROWE</b></p>
+                    <div class="tekst"><i><p class="subtext">
+                      W tym dziale poznasz podstawowe pojęcia związane
+                      z układami cyfrowymi, nauczysz się posługiwac innymi
+                      systemami liczbowymi, projektowac układy z wykorzystaniem
+                      bramek logicznych i innych komponentów</br></br></br></br>
+                    </p></i></div>
+              </div></div>
+            </a>
+
+            <!-- KUS -->
+            <div class="tile" style="background-color: #e3b307;"> <div class="inside-text">
+                <div class="image"> <img src="img/device.png" height="100%"></div>
+                <p style="margin-top: 3%; margin-bottom: 4%; color:#7e5800;"><b>KONFIGURACJA</br>URZĄDZEŃ SIECIOWYCH</b></p>
+                    <div class="tekst"><i><p class="subtext">
+                      W tym dziale nauczysz się konfigurowac switche,
+                      routery i inne urządzenia sieciowe, projektowac
+                      własne sieci lokale, dzielic je na VLANy i
+                      personalizowac do swoich potrzeb</br></br></br></br>
+                    </p></i></div>
+            </div></div>
+
+            <!-- UEA -->
+            <div class="tile" style="background-color: #cbe215"><div class="inside-text">
+                <div class="image"> <img src="img/wzmacniacz.png" height="100%"></div>
+                <p style="margin-top: 3%; margin-bottom: 4%; color:#6b770b;"><b>UKŁADY ANALOGOWE</b></p>
+                  <div class="tekst"><i><p class="subtext">
+                    W tym dziale nie nauczysz się niczego przydatnego.</br>
+                    Sorry, uczył nas Małyska :P
+                  </p></i></div>
+            </div></div>
+
+            <!-- SK/AUK -->
+            <div class="tile" style="background-color: #399e16"><div class="inside-text">
+                <div class="image"> <img src="img/link.png" height="100%"></div>
+                <p style="margin-top: 3%; margin-bottom: 4%; color:#005b00;"><b>SIECI I URZĄDZENIA</br>KOMUTACYJNE</b></p>
+                  <div class="tekst"><i><p class="subtext">
+                    OPIS
+                  </p></i></div>
+            </div></div>
+
+            <!-- PEE -->
+            <div class="tile" style="background-color: #4474d0"><div class="inside-text">
+                <div class="image"> <img src="img/uziemienie.png" height="100%"></div>
+                <p style="margin-top: 3%; margin-bottom: 4%; color:#1a366b;"><b>ELEKTROTECHNIKA</b></p>
+                  <div class="tekst"><i><p class="subtext">
+                    W tym dziale poznasz podstawowe prawa
+                    rządzące światem elektrotechniki i nauczysz
+                    się rozwiązywac zadania z obwodami elektrycznymi
+                  </p></i></div>
+            </div></div>
+
+            <!-- USTK -->
+            <div class="tile" style="background-color: #ae0ac8"><div class="inside-text">
+                <div class="image"> <img src="img/mysz.png" height="100%"></div>
+                <p style="margin-top: 3%; margin-bottom: 4%; color:#560562;"><b>URZĄDZENIA I SYSTEMY</br>KOMPUTEROWE</b></p>
+                  <div class="tekst"><i><p class="subtext">
+                    OPIS
+                  </p></i></div>
+            </div></div>
+
+            <!-- SSO -->
+            <div class="tile" style="background-color: #e754c6"><div class="inside-text">
+                <div class="image"> <img src="img/server.png" height="100%"></div>
+                <p style="margin-top: 3%; margin-bottom: 4%; color:#9c167e;"><b>SIECIOWE SYSTEMY</br>OPERACYJNE</b></p>
+            </div></div>
+
+            <!-- PLAN -->
+            <div class="tile" style="background-color: #c87137;"> <div class="inside-text">
+                <div class="image"> <img src="img/router2.png" height="100%"></div>
+                <p style="margin-top: 3%; margin-bottom: 4%; color:#784421;"><b>PODSTAWY</br>SIECI LAN</b></p>
+            </div></div>
+
+            <!-- RLAN -->
+            <div class="tile" style="background-color: #ac9d93;"> <div class="inside-text">
+                <div class="image"> <img src="img/rj45.png" height="100%"></div>
+                <p style="margin-top: 3%; margin-bottom: 4%; color:#483e37;"><b>REALIZACJA</br>SIECI LAN</b></p>
+            </div></div>
+
+            <!-- PT/LSB -->
+            <div class="tile" style="background-color: #6c5367;"> <div class="inside-text">
+                <div class="image"> <img src="img/miernik.png" height="100%"></div>
+                <p style="margin-top: 3%; margin-bottom: 4%; color:#241c22;"><b>TELETRANSMISJA</b></p>
+            </div></div>
+          </div>
+       </div>
+      </div>
+      <div class="footer_subtriangle" style="
+        border-left: 0 solid transparent;
+        border-right: 98.8vw solid transparent;
+        border-bottom: 100px solid #222930;
+        margin-top: -600px;
+        ">
+      </div>
+      <div class="footer_triangle" style="
+        border-left: 98.8vw solid transparent;
+        border-right: 0 solid transparent;
+        border-bottom: 100px solid #1a2025;
+        margin-top: -100px;">
+      </div>
+      <div class="footer_subrectangle">
+        <div class="footer_rectangle">
+          <div class="footer_rectangle_text">
+          </br><h1>Projekt TELEDU</h1>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris non libero neque. Sed vitae accumsan erat. Proin non sapien id tellus porta tempus. Aliquam interdum est id orci condimentum, a pretium felis efficitur.
+          </div>
+          <div class="footer_rectangle_text">
+          </br><h1>Cel strony</h1>roin dignissim sodales justo, vitae ultricies quam viverra non. Suspendisse tempus nec ipsum ac sagittis. Pellentesque erat justo, condimentum ullamcorper molestie at, elementum id nunc. Duis felis erat, euismod mattis pretium quis, ultricies ac diam. Nulla erat mi, ultricies at consectetur sit amet, egestas sit amet nisi.
+          </div>
+          <div class="footer_rectangle_text">
+          </br><h1>Coś o nas</h1>Vestibulum lobortis interdum faucibus. Phasellus et arcu lobortis, interdum augue et, luctus ligula. Etiam nec erat id tortor volutpat vulputate. Donec nisi neque, vulputate nec massa mattis, fermentum accumsan elit.
+          </div>
+        </div>
+      </div>
+
+      <div class="footer_subtriangle" style="
+        border-left: 98.8vw solid transparent;
+        border-right: 0 solid transparent;
+        border-top: 100px solid #222930;
+        ">
+      </div>
+      <div class="footer_triangle" style="
+        border-left: 0 solid transparent;
+        border-right: 98.8vw solid transparent;
+        border-top: 100px solid #1a2025;
+        margin-top: -100px;">
+      </div>
+
+          <div class="footer-container" style="width: 100%;">
+
+            *jakiś tekst o tym że strona była tworzona z pasją</br>
+            <p3 style="float: right; text-align: left; margin-right: 20px;">
+              @created by Kacper Murat & Piotr Paszko</br>
+              @graphics created by Kacper Murat & Piotr Paszko</br>
+              support:    <u><i>support@teledu.pl</i></u>
+            </p3>
+          </div>
+    </div>
+    <script>
+    $(document).ready(function() {
+      /*  $(window).scroll( function(){
+            $('.footer_rectangle').each( function(i){
+                var bottom_of_object = $('.footer_rectangle').offset().top + $('.footer_rectangle').outerHeight();
+                var bottom_of_window = $(window).scrollTop() + $(window).height();
+                if( bottom_of_window > bottom_of_object ){
+                    $('.footer_subtriangle').animate({'opacity':'1'},500);
+                    $('.footer_subrectangle').animate({'opacity':'1'},500);
+                    $('.footer_rectangle').delay(300).animate({'opacity':'1'},500);
+                    $('.footer_triangle').delay(300).animate({'opacity':'1'},500);
+                    $('.footer_rectangle_text').delay(500).showDown('slow');}
+                }
+            });
+        });*/
+        document.getElementById("footer-container").innerHTML = "Hello World";
+    });
+    </script>
+  </body>
+</html>
---
+++ myScript.js
@@ -0,0 +1,94 @@
+function funkcja1(ident)
+{
+  for (i = 0; i < 6; i++)
+  {
+      $("#"+i+"1").removeClass("rectangle_long");
+      if(i!=(ident-1)/10) $("#content_"+i+"1").fadeOut(600).addClass("main_content_display");
+      $("#title_"+i+"1").fadeOut(600);
+      document.getElementById("title_"+i+"1").style.display="none";
+      //document.getElementById("img_"+i+"1").style.display="block";
+      document.getElementById("img_"+i+"1").style.float="";
+
+  }
+    $("#title_"+ident).fadeIn(600);
+    document.getElementById("title_"+ident).style.display="block";
+    //document.getElementById("img_"+ident).style.display="none";
+    document.getElementById("img_"+ident).style.float="left";
+  $("#"+ident).addClass("rectangle_long");
+/*  $("#content_"+ident).removeClass("main_content_display").fadeIn(600);*/
+  if(ident=='01') $("main_choose").addClass("main_choose_display");
+  else $("main_choose").removeClass("main_choose_display");
+}
+
+function choose_menu(iden)
+{
+  $("#choose_option_1").addClass("choose_option_off");
+  $("#choose_option_2").addClass("choose_option_off");
+  $("#choose_option_3").addClass("choose_option_off");
+  $("#choose_option_"+iden).removeClass("choose_option_off");
+  $("#choose_option_"+iden).addClass("choose_option_on");
+
+
+}
+
+function update_content(section, ident)
+{
+    $("#"+section+ident).load("content/" + section + "/" + ident + ".html");
+    $('#UEC1').load("content/UEC/1.html");
+}
+
+/*$(document).ready(function() {
+  $("#choose_option_1").click(function() {
+    $('#UEC1').load("content/UEC/1.html");
+});
+});*/
+
+
+/*
+var cssLink = document.createElement("link")
+cssLink.href = "style.css";
+cssLink.rel = "stylesheet";
+cssLink.type = "text/css";
+document.iframe.body.appendChild(cssLink);
+
+
+frame1.$('mydiv').addClassName('withborder')
+
+frame1.$('mydiv').style.border = '1px solid #000000'
+*/
+
+
+/*
+$('iframe').load( function() {
+  $('iframe').contents().find("head")
+  .append($("<style type='text/css'>  .main_content{color:red;}  </style>"));
+});
+
+function horizontal_menu(iden)
+{
+  document.getElementById("#choose_option_1").style.background-color="rgba(255, 255, 255, 0.08)";
+  document.getElementById("#choose_option_2").style.background-color="rgba(255, 255, 255, 0.08)";
+  document.getElementById("#choose_option_3").style.background-color="rgba(255, 255, 255, 0.08)";
+  document.getElementById("#choose_option_1").style.background-color="#201f28";
+}<!--onload="funkcja1('01')"-->
+*/
+
+
+/*funcadeIn(300);
+  //if(ident=='01') $("main_choose").addClass("main_choose_display");
+  //else $("main_choose").removeClass("main_choose_display");
+  for (i = 1; i < 4; i++)
+  {
+      $("#choose_option_"+iden).addClass("choosen_button");
+  }
+  $("#choose_option_1").removeClass("choosen_button");
+  $("#choose_option_2".removeClass("choosen_button");
+  $("#choose_option_3").removeClass("choosen_button");
+  $("#choose_option_"+iden).addClass("choosen_button");
+}*/
+/*setTimeout("dodaj()",500)
+
+link.setAttribute("type", "text/css");
+
+*/
+ 
